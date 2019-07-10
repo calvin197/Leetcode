2174ac403f2c21fc246d4d2f81e8f37d16f53ff6
Calvin <calvinshum1874@gmail.com>
2019-07-09 18:22:12 -0700

    91 finished but look so ugly

--- 91. Decode Ways.js
+++ 91. Decode Ways.js
@@ -1,11 +1,23 @@
 var numDecodings = function(s) {
-    if (s[0]==='0') return 0; 
-    let storage = {}; 
-    for (let i=s.length; i>=0; i--){
-        if (i===s.length) storage[i] = 1; 
-        else {
-            storage[i] = ((typeof storage[i+1] === 'number') && (Number(s.slice(i,i+1))<=26) && Number(s.slice(i,i+1))>0 ? storage[i+1] : 0) + ((typeof storage[i+2] === 'number') && (Number(s.slice(i,i+2))<=26) && (Number(s.slice(i,i+2))>0)? storage[i+2] : 0); 
-        }
+  if (s[0] === "0") return 0;
+  let storage = {};
+  for (let i = s.length; i >= 0; i--) {
+    if (i === s.length) storage[i] = 1;
+    else {
+      storage[i] =
+        (typeof storage[i + 1] === "number" &&
+        Number(s.slice(i, i + 1)) <= 26 &&
+        Number(s.slice(i, i + 1)) > 0 &&
+        s.slice(i, i + 1)[0] !== "0"
+          ? storage[i + 1]
+          : 0) +
+        (typeof storage[i + 2] === "number" &&
+        Number(s.slice(i, i + 2)) <= 26 &&
+        Number(s.slice(i, i + 2)) > 0 &&
+        s.slice(i, i + 2)[0] !== "0"
+          ? storage[i + 2]
+          : 0);
     }
-    return storage[0];
+  }
+  return storage[0];
 };
