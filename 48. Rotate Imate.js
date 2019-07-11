ba19af3bf5f2d07c2850a51c67c0e8586662835f
Calvin <calvinshum1874@gmail.com>
2019-07-11 08:57:28 -0700

    redo 48, rotate image. the solution is more readable now

--- 48. Rotate Imate.js
+++ 48. Rotate Imate.js
@@ -1,4 +1,26 @@
 var rotate = function(matrix){
+  for (let i = 0; i < Math.floor(matrix.length/2); i++){
+    let startIdx = i;
+    let endIdx = matrix.length-1-i;
+    for (let j = 0; j < endIdx-startIdx; j++){
+      // swap top left and top right
+      swap(matrix, startIdx, startIdx+j, startIdx+j, endIdx);
+      // swap top left and bottom right
+      swap(matrix, startIdx, startIdx+j, endIdx, endIdx-j);
+      // swap top left and bottom left
+      swap(matrix, startIdx, startIdx+j, endIdx-j, startIdx)
+    }
+  }
+}
+
+var swap = function(matrix, a1, a2, b1, b2){
+  [matrix[a1][a2], matrix[b1][b2]] = [ matrix[b1][b2], matrix[a1][a2]];
+}
+
+
+
+
+var rotate = function(matrix){
   let total = Math.floor(matrix.length / 2);
   let start = 0; 
   while (total > 0){
